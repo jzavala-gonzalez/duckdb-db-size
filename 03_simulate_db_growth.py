4b21f4b89f95f6d2db389f31eb42cde2c8433447
José M. Zavala González <jose.m.zavala.gonzalez@gmail.com>
2024-01-25 13:32:24 -0400

    clean up plot

--- 03_simulate_db_growth.py
+++ 03_simulate_db_growth.py
@@ -8,7 +8,7 @@ from constants import data_dir
 num_starting_files_options = [1000, 3000, 5000, 7000, 9000,
                               ] # how many files ingested at creation time
                                    # must be at least 1
-batch_size = 2000 # how many files to ingest at a time, usually 1 or 2
+batch_size = 500 # how many files to ingest at a time, usually 1 or 2
 
 # Get file info
 con_files = duckdb.connect('databases/files.db')
--- 04_plot_results.py
+++ 04_plot_results.py
@@ -33,7 +33,7 @@ sizes_df_initial_num_files = sizes_df_initial.select('num_files').to_series().to
 sizes_df_initial_mb = sizes_df_initial.select('database_size_mb').to_series().to_list()
 
 distinct_starting_files = sizes_df_initial_num_files # same thing
-
+batch_size = sizes_df_initial.select('batch_size').to_series().to_list()[0] # Assuming constant
 # fig = px.line(sizes_df,
 #                  y='database_size_mb',
 #                  x='num_files',
@@ -53,23 +53,43 @@ fig = go.Figure()
 
 
 
-for num_starting_files in distinct_starting_files:
+for i,num_starting_files in enumerate(distinct_starting_files):
     sizes_df_subset = sizes_df.filter(pl.col('num_starting_files') == num_starting_files)
     sizes_df_subset_num_files = sizes_df_subset.select('num_files').to_series().to_list()
     sizes_df_subset_mb = sizes_df_subset.select('database_size_mb').to_series().to_list()
-
-    fig.add_trace(go.Scatter(x=sizes_df_subset_num_files, y=sizes_df_subset_mb,
+    sizes_df_subset_num_batches_added = sizes_df_subset.select('num_batches_added').to_series().to_list()
+    fig.add_trace(go.Scatter(
+        x=sizes_df_subset_num_files, y=sizes_df_subset_mb,
+        text=sizes_df_subset_num_batches_added,
                     mode='lines+markers',
-                    name='lines+markers',
+                    name='Batched ingest',
                     line=dict(color='grey', width=1),
+                    showlegend= i==0,
+                    hoverlabel_bgcolor='white',
+                    hovertemplate =
+                        '<b>Files</b>: %{x:,}'+
+                        '<br><b>Size</b>: %{y:.2f}MB<br>'
+                        +f'<b>Initial files</b> {num_starting_files:,}<br>'
+                        +'<b>Batches ingested</b> %{text:,}',
                 ))
     pass
 
 fig.add_trace(go.Scatter(x=sizes_df_initial_num_files, y=sizes_df_initial_mb,
                     mode='lines+markers',
-                    name='lines+markers'))
+                    name='Initial ingest',
+                    hovertemplate =
+                        '<b>Initial files</b>: %{x:,}'+
+                        '<br><b>Size</b>: %{y:.2f}MB<br>'
+                    ))
 # fig.add_trace(go.Scatter(x=random_x, y=random_y2,
 #                     mode='lines',
 #                     name='lines'))
+
+fig.update_layout(
+    title=f'DuckDB database size vs. number of files<br>(batch size = {batch_size:,} files)',
+    xaxis_title='Number of files',
+    xaxis_tickformat = ',',
+    yaxis_title='Database file size (MB)',
+)
 
 fig.show()
